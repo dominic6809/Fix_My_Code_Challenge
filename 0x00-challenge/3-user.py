2604baa9c971b634d66d189db66446b0883bbc83
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-26 18:03:54 +0300

    user test

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
- User Model
-"""
+"""User Model"""
+
 import hashlib
 import uuid
 
@@ -13,13 +12,14 @@ class User():
     - password: private string hash in MD5
     """
 
+    __password = None
+
     def __init__(self):
         """
         Initialize a new user:
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
-        self.__password = None
 
     @property
     def password(self):
@@ -51,9 +51,9 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
@@ -84,11 +84,12 @@ if __name__ == '__main__':
         print("User.password should be None if setter to an integer")
 
     if not user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right password")
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return
-                False if it's not the right password")
+        print("is_valid_password should return False if it's not the right \
+password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -97,4 +98,5 @@ if __name__ == '__main__':
         print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no password set before")
+        print("is_valid_password should return False if no password set \
+before")
