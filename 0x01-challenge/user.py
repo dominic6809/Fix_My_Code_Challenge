4b164f58708a291ddad44cf1ab001a1babca7dc1
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-21 19:48:14 +0300

    Update user.py

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -6,10 +6,17 @@ User class
 class User():
     """ Documentation """
 
+    __email = None
+
     def __init__(self):
         """ Documentation """
         self.__email = None
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+    
     @email.setter
     def email(self, value):
         """ Documentation """
@@ -17,10 +24,7 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
+    
    
     
 if __name__ == "__main__":
